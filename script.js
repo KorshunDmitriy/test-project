6b36d7c406ca6adf5fce2c82626fc87e768fdac7
KorshunDmitriy <75948030+KorshunDmitriy@users.noreply.github.com>
2020-12-14 23:47:14 +0200

    Update script.js
    12

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-// "use strict";
+'use strict';
 
-const _true =  false,
-      _false = true;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-console.log(_true || _false);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
